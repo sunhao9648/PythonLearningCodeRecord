83bd03048a0a5f4fc37c753101dac523688234f8
sunhao9648 <49889698+sunhao9648@users.noreply.github.com>
2019-04-23 15:06:39 +0800

    Add files via upload

---
+++ FindUse.py
@@ -0,0 +1,20 @@
+s = "1w335ty65452dlqfs"
+s1 = "5"
+print(s.find(s1))
+sum = s.find(s1)
+print(s.find(s1,sum+1))
+
+a1 = "abcde"
+a2 = ""
+a3 = "祝你生日快乐"
+print(a1.isalpha())
+print(a2.isalpha())
+print(a3.isalpha())
+
+b1 = "QAW"
+b2 = "qwd"
+print(b1.islower())
+print(b2.islower())
+print(b1.isupper())
+print(b2.isupper())
+print(b1.format(b2))
